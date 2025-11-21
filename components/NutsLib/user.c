4fe5b6bb3be1f62f1c892e6f9b57b6488c040170
HUANsic <wu-zh20@mails.tsinghua.edu.cn>
2025-11-21 19:32:49 +0800

    removed aes part

--- components/NutsLib/user.c
+++ components/NutsLib/user.c
@@ -33,81 +33,10 @@ NutStatus_e Echo(uint8_t *received_data_ptr, uint32_t received_data_length, uint
 	return NUT_OK;
 }
 
-/* mbedTLS AES */
-#include "mbedtls/aes.h"
-
-mbedtls_aes_context aes_ctx;
-#define KEY_LENGTH 128
-
-NutStatus_e AES_SetEncryptionKey(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
-								 uint32_t result_buffer_MAX_size)
-{
-	*result_length = 0;
-	if (mbedtls_aes_setkey_enc(&aes_ctx, received_data_ptr, KEY_LENGTH))
-		return NUT_ERROR;
-	return NUT_OK;
-}
-
-NutStatus_e AES_SetDecryptionKey(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
-								 uint32_t result_buffer_MAX_size)
-{
-	*result_length = 0;
-	if (mbedtls_aes_setkey_dec(&aes_ctx, received_data_ptr, KEY_LENGTH))
-		return NUT_ERROR;
-	return NUT_OK;
-}
-
-NutStatus_e AES_Encrypt(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
-						uint32_t result_buffer_MAX_size)
-{
-	*result_length = 16;
-	// Nut_Quiet();
-	Nut_LED(1);
-	Nut_Trigger_Set();
-	int status = mbedtls_aes_crypt_ecb(&aes_ctx, MBEDTLS_AES_ENCRYPT, received_data_ptr, result_buffer_ptr);
-	Nut_Trigger_Clear();
-	Nut_LED(0);
-	// Nut_Unquiet();
-	if (status == 0)
-	{
-		return NUT_OK;
-	}
-	else
-	{
-		return NUT_ERROR;
-	}
-}
-
-NutStatus_e AES_Decrypt(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
-						uint32_t result_buffer_MAX_size)
-{
-	*result_length = 16;
-	// Nut_Quiet();
-	Nut_LED(1);
-	Nut_Trigger_Set();
-	int status = mbedtls_aes_crypt_ecb(&aes_ctx, MBEDTLS_AES_DECRYPT, received_data_ptr, result_buffer_ptr);
-	Nut_Trigger_Clear();
-	Nut_LED(0);
-	// Nut_Unquiet();
-	if (status == 0)
-	{
-		return NUT_OK;
-	}
-	else
-	{
-		return NUT_ERROR;
-	}
-}
-
 /* User command */
 // @formatter:off
 NutAction_t command_list[] = {
 	{.command = 0x0001, .function = Echo},
-	/* mbedTLS AES */
-	{.command = 0x0100, .function = AES_SetEncryptionKey},
-	{.command = 0x0101, .function = AES_SetDecryptionKey},
-	{.command = 0x0102, .function = AES_Encrypt},
-	{.command = 0x0103, .function = AES_Decrypt},
 };
 // @formatter:on
 uint16_t command_count = sizeof(command_list) / sizeof(command_list[0]);
@@ -115,7 +44,6 @@ uint16_t command_count = sizeof(command_list) / sizeof(command_list[0]);
 void User_Init()
 {
 	ESP_LOGI(TAG, "User Initialization start.");
-	mbedtls_aes_init(&aes_ctx);
 	Nut_LED(1);
 	usleep(200000);
 	Nut_LED(0);
