433afba605917c9b6ed232166546a35b80c7262e
HUANsic <wu-zh20@mails.tsinghua.edu.cn>
2026-04-09 11:39:35 +0800

    tested, ok

--- components/NutsLib/user.c
+++ components/NutsLib/user.c
@@ -34,11 +34,47 @@ NutStatus_e Echo(uint8_t *received_data_ptr, uint32_t received_data_length, uint
 }
 
 #include "esp_sleep.h"
+#include "driver/gpio.h"
+#include <rom/ets_sys.h>
+
+void set_all_pins_pullup()
+{
+	gpio_set_direction(NUT_UART_TX_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_UART_RX_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_SPI_CS_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_SPI_SCK_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_SPI_MOSI_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_SPI_MISO_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_I2C_SCL_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_I2C_SDA_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_LED_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_IO1_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_IO2_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_IO3_PIN, GPIO_MODE_INPUT);
+	gpio_set_direction(NUT_IO_USER_PIN, GPIO_MODE_INPUT);
+	gpio_pullup_en(NUT_UART_TX_PIN);
+	gpio_pullup_en(NUT_UART_RX_PIN);
+	gpio_pullup_en(NUT_SPI_CS_PIN);
+	gpio_pullup_en(NUT_SPI_SCK_PIN);
+	gpio_pullup_en(NUT_SPI_MOSI_PIN);
+	gpio_pullup_en(NUT_SPI_MISO_PIN);
+	gpio_pullup_en(NUT_I2C_SCL_PIN);
+	gpio_pullup_en(NUT_I2C_SDA_PIN);
+	gpio_pullup_en(NUT_LED_PIN);
+	gpio_pullup_en(NUT_IO1_PIN);
+	gpio_pullup_en(NUT_IO2_PIN);
+	gpio_pullup_en(NUT_IO3_PIN);
+	gpio_pullup_en(NUT_IO_USER_PIN);
+}
 
 NutStatus_e EnterLightSleep_NoWake(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
 								   uint32_t result_buffer_MAX_size)
 {
 	*result_length = 0;
+	Nut_LED(1);
+	ets_delay_us(100000);
+	Nut_LED(0);
+	set_all_pins_pullup();
 	esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
 	esp_light_sleep_start();
 	return NUT_OK;
@@ -48,6 +84,18 @@ NutStatus_e EnterDeepSleep_NoWake(uint8_t *received_data_ptr, uint32_t received_
 								  uint32_t result_buffer_MAX_size)
 {
 	*result_length = 0;
+	Nut_LED(1);
+	ets_delay_us(100000);
+	Nut_LED(0);
+	ets_delay_us(100000);
+	Nut_LED(1);
+	ets_delay_us(100000);
+	Nut_LED(0);
+	ets_delay_us(100000);
+	Nut_LED(1);
+	ets_delay_us(100000);
+	Nut_LED(0);
+	set_all_pins_pullup();
 	esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
 	esp_deep_sleep_start();
 	return NUT_OK;
